8babf1d277eb380a48e39cb0e3ae70f3f167efdd
Alexandros Philotheou <alexandros.filotheou@gmail.com>
2026-01-11 11:48:25 +0200

    LICENSE: MIT->BSD

--- src/pointcloud_to_ply_node.cpp
+++ src/pointcloud_to_ply_node.cpp
@@ -3,8 +3,8 @@
  *
  * Copyright (c) 2025 Alexandros PHILOTHEOU
  *
- * Licensed under the MIT License.
- * See LICENSE.MIT for details.
+ * Licensed under the BSD License.
+ * See LICENSE for details.
  */
 
 #include <rclcpp/rclcpp.hpp>
